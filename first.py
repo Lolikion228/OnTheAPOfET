d04ca5a544c6eb3b5da6a1120788a12b518536d1
Lolikion228 <st117139@student.spbu.ru>
2025-10-12 20:39:15 +0300

    first ver of integrals

--- first.py
+++ first.py
@@ -3,14 +3,45 @@ from scipy.integrate import nquad
 
 
 def compute_integrals(f, g, d2_g, h1, h2):
-    pass
+    integrals = dict()
+
+    integrals["J1"] = nquad(
+        lambda x,y: g(x - y) * f(x) * f(y),
+        ranges=[(-np.inf, +np.inf),
+                (-np.inf, +np.inf)])
+    
+    integrals["J2"] = nquad(
+        lambda x,y: (g(x - y) ** 2) * f(x) * f(y),
+        ranges=[(-np.inf, +np.inf),
+                (-np.inf, +np.inf)])
+    
+    integrals["J3"] = nquad(
+        lambda x,y,z: g(x - y) * g(x - z) * f(x) * f(y) * f(z),
+        ranges=[(-np.inf, +np.inf),
+                (-np.inf, +np.inf),
+                (-np.inf, +np.inf)])
+    
+    integrals["J1_star"] = 0.5 * (h1 ** 2) * nquad(
+        lambda x,y: d2_g(x - y) * f(x) * f(y),
+        ranges=[(-np.inf, +np.inf),
+                (-np.inf, +np.inf)])
+    
+    integrals["J2_star"] = 0.5 * (h2 ** 2) * nquad(
+        lambda x,y: (y**2 - 0.5 * (x-y)**2) * d2_g(x - y) * f(x) * f(y),
+        ranges=[(-np.inf, +np.inf),
+                (-np.inf, +np.inf)])
+    
+    
+    return integrals
 
 
 def compute_asymptotic_power(alpha, b, a):
     pass
 
+
 def compute_crit_val(n, M, alpha):
     pass
+
 
 def compute_empirical_power(n, N, crit_val):
     pass
--- main.py
+++ main.py
@@ -14,12 +14,13 @@ def f(x):
 
 
 
-np.random
 
 def run_experiment(f, g, d2_g, 
                    h1=0, h2=2.1, alpha=0.05,
                    N=1000, M=17, sample_sizes=[100, 400, 900]):
     """
+    params:
+    ---
     ``h1`` - scale diff
 
     ``h2`` - shift diff
@@ -32,27 +33,29 @@ def run_experiment(f, g, d2_g,
 
     ``sample_sizes`` - sizes of samples to compute empirical power
 
+    return:
+    ---------
+    ``empirical_powers`` and ``asymptotic_power``
     """
+
     integrals = compute_integrals(f, g, d2_g, h1, h2)
 
-    b1 = ...
-    b2 = ...
+    b1 = np.sqrt( np.abs( integrals["J1_star"] ) )
+    b2 = np.sqrt( np.abs( integrals["J2_star"] ) )
     
-    a = ... 
-    b = ...
+    a = (integrals["J2"] + integrals["J1"] ** 2 \
+         - 2 * integrals["J3"]) ** 0.25
+    b = np.sqrt( b1 ** 2 + b2 ** 2)
 
-    a_pow = compute_asymptotic_power(alpha, b, a)
-
+    emp_powers = []
+    asp_power = compute_asymptotic_power(alpha, b, a)
     
-    e_pows = []
-
     for n in sample_sizes:
         crit_val = compute_crit_val(n, M, alpha)
         e_pow = compute_empirical_power(n, N, crit_val)
-        e_pows.append(e_pow)
+        emp_powers.append(e_pow)
 
-    return e_pow, a_pow
-
+    return emp_powers, asp_power
 
 
 def main():
