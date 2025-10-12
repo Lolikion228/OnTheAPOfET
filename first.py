e73561e2c6f130cc59842416f0491f8cae551d7c
Lolikion228 <st117139@student.spbu.ru>
2025-10-12 19:53:53 +0300

    init

---
+++ first.py
@@ -0,0 +1,18 @@
+import numpy as np
+from scipy.integrate import nquad
+
+
+def compute_integrals(f, g, d2_g, h1, h2):
+    pass
+
+
+def compute_asymptotic_power(alpha, b, a):
+    pass
+
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,21 @@
+import first
+
+
+
+def g(x):
+    pass
+
+# density for F(x)
+def f(x):
+    pass
+
+
+
+def main():
+    h1 = 0
+    h2 = 2.1
+
+    
+
+
+main()
