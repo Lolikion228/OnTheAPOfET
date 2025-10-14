4fd13efff340749ad913dab2cd7ce02cc438747f
Lolikion228 <st117139@student.spbu.ru>
2025-10-14 21:06:17 +0300

    add compute_asymptotic_power

--- first.cpp
+++ first.cpp
@@ -1,5 +1,6 @@
 #include "boost/random/cauchy_distribution.hpp"
 #include "boost/random/normal_distribution.hpp"
+#include "boost/math/distributions.hpp"
 #include <boost/random.hpp>
 #include <iostream>
 
@@ -23,6 +24,12 @@ double compute_etest(std::function<double(double)> g, double *X, double *Y, int 
     return (phi_ab - phi_a - phi_b) / (sample_size * sample_size);
 }
 
+
+double compute_asymptotic_power(double alpha, double b, double a){
+    boost::math::normal_distribution<> dist(0, 1);
+    double z = boost::math::quantile(dist, 1 - alpha / 2);
+    return 1 - boost::math::cdf(dist, z - b / a) + boost::math::cdf(dist, -z - b / a);
+}
 
 
 void print_sample(double *sample, int sample_size){
